5dc5feda577ab6d9d02e46e0b04407b264b972d4
Christian Stanfield <christian.stanfield@gmail.com>
2015-03-09 18:11:39 -0500

    circle radius's updating on zoom

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -5,15 +5,18 @@ function loadRouteMap() {
     { zoom: 11,
       center: new google.maps.LatLng(41.881832, -87.623177)
     });
+  var mapZoom = 11; // for adjusting circle radius on zoom
 
   // Circles
+  var circles = [];
+
   for (var i = 0; i < gon.bus_stops.length; i++) {
     var bus_stop = gon.bus_stops[i];
 
     var circleOptions = {
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
+      // strokeColor: '#FF0000',
+      // strokeOpacity: 0.8,
+      strokeWeight: 0,
       fillColor: '#FF0000',
       fillOpacity: 0.35,
       map: map,
@@ -21,8 +24,25 @@ function loadRouteMap() {
       radius: bus_stop.boardings
     };
 
-    var cityCircle = new google.maps.Circle(circleOptions);
+    circles.push(new google.maps.Circle(circleOptions));
   }
+
+  // update circles' radius on zoom
+  google.maps.event.addListener(map, 'zoom_changed', function() {
+    var newRadius;
+
+    if (map.zoom > mapZoom) {
+      newRadius = 0.5;
+      mapZoom = map.zoom;
+    } else {
+      newRadius = 2;
+      mapZoom = map.zoom;
+    }
+
+    for (var i = 0; i < circles.length; i++) {
+      circles[i].setRadius(circles[i].radius * newRadius);
+    }
+  });
 }
 
 if (window.location.pathname === '/routes/' + gon.route.id) google.maps.event.addDomListener(window, 'load', loadRouteMap);
